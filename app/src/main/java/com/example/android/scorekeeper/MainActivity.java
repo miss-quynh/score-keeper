def932397eddd30be2e13903b09d990a4311be93
Quynh Nguyen <qnguyenx3@gmail.com>
2018-05-15 21:56:17 -0700

    Add library to blur image

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.android.scorekeeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jgabrielfreitas.core.BlurImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BlurImageView backgroundImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backgroundImage = (BlurImageView) findViewById(R.id.backgroundImage);
+        backgroundImage.setBlur(2);
     }
 }
